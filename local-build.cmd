0414f4dfb3fa083109ce83f4da5a6cb7b874206c
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-05 04:50:51 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -2,6 +2,9 @@
 
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 
+set LLVM_BIN_DIR=%DOWNLOADS_DIR%\LLVM-21.1.2-win64\bin
+set CLANG_FORMAT_EXE=%LLVM_BIN_DIR%\clang-format.exe
+
 set GOROOT=%DOWNLOADS_DIR%\go1.21.0.windows-amd64\go
 set GOPATH=%DOWNLOADS_DIR%\gopath
 set GOBIN=%GOROOT%\bin
@@ -13,3 +16,4 @@ set PATH=^
 %GOBIN%;
 
 go build main.go &&^
+xcopy /H /Y /C "%CLANG_FORMAT_EXE%"
