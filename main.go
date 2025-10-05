ba4f210b2b58ebd174cef6a89d849909dd8966af
dirkarnez <smalldirkalex@gmail.com>
2025-10-05 22:54:44 +0800

    - wip

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 var (
 	clangFormatExePath string
-	clangStylePath     string
+	// clangStylePath     string
 )
 
 func pushCommentToRight(line string, width int) string {
@@ -35,27 +35,27 @@ func pushCommentToRight(line string, width int) string {
 			re := regexp.MustCompile("\t+")
 			tabPrefixOccurance := len(re.FindString(codePart))
 			lineLength = lineLength + (tabPrefixOccurance * 7)
-			// codePart = fmt.Sprintf("%s%s", strings.Repeat(" ", tabPrefixOccurance*4), strings.TrimLeft(codePart, "\t"))
+			codePart = fmt.Sprintf("%s%s", strings.Repeat(" ", tabPrefixOccurance*4), strings.TrimLeft(codePart, "\t"))
 		}
 
-		if strings.Contains(strings.TrimPrefix(codePart, "\t"), "\t") {
-			re := regexp.MustCompile("\t+")
-			for _, item := range re.FindAllString(strings.TrimPrefix(codePart, "\t"), -1) {
-				tabPrefixOccurance := len(item)
-				lineLength = lineLength + (tabPrefixOccurance * 5)
-			}
-			// codePart = fmt.Sprintf("%s%s", strings.Repeat(" ", tabPrefixOccurance*4), strings.TrimLeft(codePart, "\t"))
-		}
+		// if strings.Contains(strings.TrimPrefix(codePart, "\t"), "\t") {
+		// 	re := regexp.MustCompile("\t+")
+		// 	for _, item := range re.FindAllString(strings.TrimPrefix(codePart, "\t"), -1) {
+		// 		tabPrefixOccurance := len(item)
+		// 		lineLength = lineLength + (tabPrefixOccurance * 5)
+		// 	}
+		// 	// codePart = fmt.Sprintf("%s%s", strings.Repeat(" ", tabPrefixOccurance*4), strings.TrimLeft(codePart, "\t"))
+		// }
 
-		if strings.HasPrefix(codePart, " ") {
-			re := regexp.MustCompile(`\s+`)
-			spacePrefixOccurance := len(re.FindString(codePart))
-			if spacePrefixOccurance%4 == 0 {
-				numberOfTabs := int(spacePrefixOccurance / 4)
-				codePart = fmt.Sprintf("%s%s", strings.Repeat("\t", numberOfTabs), strings.TrimLeft(codePart, " "))
-				lineLength = lineLength - spacePrefixOccurance + (numberOfTabs * 8)
-			}
-		}
+		// if strings.HasPrefix(codePart, " ") {
+		// 	re := regexp.MustCompile(`\s+`)
+		// 	spacePrefixOccurance := len(re.FindString(codePart))
+		// 	if spacePrefixOccurance%4 == 0 {
+		// 		numberOfTabs := int(spacePrefixOccurance / 4)
+		// 		codePart = fmt.Sprintf("%s%s", strings.Repeat("\t", numberOfTabs), strings.TrimLeft(codePart, " "))
+		// 		lineLength = lineLength - spacePrefixOccurance + (numberOfTabs * 8)
+		// 	}
+		// }
 
 		if lineLength < width {
 			spacesNeeded := width - lineLength
@@ -65,21 +65,28 @@ func pushCommentToRight(line string, width int) string {
 		// Combine the code part and the comment part
 		return fmt.Sprintf("%s %s", codePart, commentPart)
 	} else {
-		if strings.HasPrefix(line, " ") {
-			re := regexp.MustCompile(`\s+`)
-			spacePrefixOccurance := len(re.FindString(line))
-			if spacePrefixOccurance%4 == 0 {
-				numberOfTabs := int(spacePrefixOccurance / 4)
-				line = fmt.Sprintf("%s%s", strings.Repeat("\t", numberOfTabs), strings.TrimLeft(line, " "))
-			}
-		}
+		// if strings.HasPrefix(line, " ") {
+		// 	re := regexp.MustCompile(`\s+`)
+		// 	spacePrefixOccurance := len(re.FindString(line))
+		// 	if spacePrefixOccurance%4 == 0 {
+		// 		numberOfTabs := int(spacePrefixOccurance / 4)
+		// 		line = fmt.Sprintf("%s%s", strings.Repeat("\t", numberOfTabs), strings.TrimLeft(line, " "))
+		// 	}
+		// }
 
+		if strings.HasPrefix(line, "\t") {
+			re := regexp.MustCompile("\t+")
+			tabPrefixOccurance := len(re.FindString(line))
+			line = fmt.Sprintf("%s%s", strings.Repeat(" ", tabPrefixOccurance*4), strings.TrimLeft(line, "\t"))
+		}
 		return line
 	}
 }
 
-func runClangFormat(folder, fileName string) error {
-	cmd := exec.Command(clangFormatExePath, fmt.Sprintf("--style=file:%s", clangStylePath), fmt.Sprintf("--files=%s", filepath.Join(folder, fileName)))
+func RunClangFormat(folder, fileName string) error {
+	// args := []string{fmt.Sprintf("--style=file:%s", clangStylePath), "-i", filepath.Join(folder, fileName)}
+	args := []string{`--style={ BasedOnStyle: LLVM,  AlignAfterOpenBracket: Align, AlignArrayOfStructures: None, AlignConsecutiveAssignments: { Enabled: false, AcrossEmptyLines: false, AcrossComments: false, AlignCompound: false, AlignFunctionDeclarations: false, AlignFunctionPointers: false, PadOperators: true }, AlignConsecutiveBitFields: { Enabled: false, AcrossEmptyLines: false, AcrossComments: false, AlignCompound: false, AlignFunctionDeclarations: false, AlignFunctionPointers: false, PadOperators: false }, AlignConsecutiveDeclarations: { Enabled: false, AcrossEmptyLines: false, AcrossComments: false, AlignCompound: false, AlignFunctionDeclarations: true, AlignFunctionPointers: false, PadOperators: false }, AlignConsecutiveMacros: { Enabled: false, AcrossEmptyLines: false, AcrossComments: false, AlignCompound: false, AlignFunctionDeclarations: false, AlignFunctionPointers: false, PadOperators: false }, AlignConsecutiveShortCaseStatements: { Enabled: false, AcrossEmptyLines: false, AcrossComments: false, AlignCaseArrows: false, AlignCaseColons: false }, AlignConsecutiveTableGenBreakingDAGArgColons: { Enabled: false, AcrossEmptyLines: false, AcrossComments: false, AlignCompound: false, AlignFunctionDeclarations: false, AlignFunctionPointers: false, PadOperators: false }, AlignConsecutiveTableGenCondOperatorColons: { Enabled: false, AcrossEmptyLines: false, AcrossComments: false, AlignCompound: false, AlignFunctionDeclarations: false, AlignFunctionPointers: false, PadOperators: false }, AlignConsecutiveTableGenDefinitionColons: { Enabled: false, AcrossEmptyLines: false, AcrossComments: false, AlignCompound: false, AlignFunctionDeclarations: false, AlignFunctionPointers: false, PadOperators: false }, AlignEscapedNewlines: Right, AlignOperands: Align, AlignTrailingComments: { Kind: Always, OverEmptyLines: 0 }, AllowAllArgumentsOnNextLine: true, AllowAllParametersOfDeclarationOnNextLine: true, AllowBreakBeforeNoexceptSpecifier: Never, AllowShortBlocksOnASingleLine: Never, AllowShortCaseExpressionOnASingleLine: true, AllowShortCaseLabelsOnASingleLine: false, AllowShortCompoundRequirementOnASingleLine: true, AllowShortEnumsOnASingleLine: true, AllowShortFunctionsOnASingleLine: All, AllowShortIfStatementsOnASingleLine: Never, AllowShortLambdasOnASingleLine: All, AllowShortLoopsOnASingleLine: false, AllowShortNamespacesOnASingleLine: false, AlwaysBreakAfterDefinitionReturnType: None, AlwaysBreakBeforeMultilineStrings: false, UseTab: Never, IndentWidth: 4, TabWidth: 4, BreakBeforeBraces: Allman, AllowShortIfStatementsOnASingleLine: false, IndentCaseLabels: false, ColumnLimit: 0, AccessModifierOffset: -4, NamespaceIndentation: All, FixNamespaceComments: false, IndentPPDirectives: BeforeHash }`, "-i", filepath.Join(folder, fileName)}
+	cmd := exec.Command(clangFormatExePath, args...)
 	err := cmd.Run()
 	if err != nil {
 		return err
@@ -87,35 +94,57 @@ func runClangFormat(folder, fileName string) error {
 	return nil
 }
 
-func Clean(folder, fileName string) error {
+func ReadFileAsLines(folder, fileName string) ([]string, error) {
 	fullPath := filepath.Join(folder, fileName)
-	fmt.Println(fullPath)
 
 	file, err := os.Open(fullPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	lines := []string{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+func Clean(folder, fileName string) error {
+	fullPath := filepath.Join(folder, fileName)
+	// fmt.Println(fullPath)
+
+	RunClangFormat(folder, fileName)
+
+	lines, err := ReadFileAsLines(folder, fileName)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(fullPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 	if err != nil {
 		return err
 	}
 
 	defer file.Close()
 
-	outputFile, err := os.Create(filepath.Join(folder, fmt.Sprintf("%s.new", fileName)))
-	if err != nil {
-		return err
-	}
-
-	defer outputFile.Close()
-
-	scanner := bufio.NewScanner(file)
-	w := bufio.NewWriter(outputFile)
+	w := bufio.NewWriter(file)
 
 	commentBlockDetected := false
 	commentBlockStartDetected := false
 	commentBlockEndDetected := false
 
 	var done string
-	for scanner.Scan() {
-		code := scanner.Text()
-		if strings.HasPrefix(code, "//=") || strings.HasPrefix(code, "//*") || strings.HasPrefix(code, "/**") || strings.HasPrefix(code, "//-") {
+	for _, code := range lines {
+		codeTrimmedSpaces := strings.TrimSpace(code)
+		if strings.HasPrefix(codeTrimmedSpaces, "//=") || strings.HasPrefix(codeTrimmedSpaces, "//*") || strings.HasPrefix(codeTrimmedSpaces, "/**") || strings.HasPrefix(codeTrimmedSpaces, "/*=") || strings.HasPrefix(codeTrimmedSpaces, "//-") {
 			commentBlockDetected = true
 
 			if commentBlockStartDetected {
@@ -125,30 +154,29 @@ func Clean(folder, fileName string) error {
 			if !commentBlockStartDetected && !commentBlockEndDetected {
 				commentBlockStartDetected = true
 			}
-		} else {
-			if commentBlockStartDetected && commentBlockEndDetected {
+		}
+
+		if commentBlockDetected {
+			if strings.HasPrefix(codeTrimmedSpaces, "//") {
+				done = code
+				if commentBlockStartDetected && commentBlockEndDetected {
+					commentBlockDetected = false
+					commentBlockStartDetected = false
+					commentBlockEndDetected = false
+				}
+			} else {
 				commentBlockDetected = false
 				commentBlockStartDetected = false
 				commentBlockEndDetected = false
 			}
 		}
 
-		if commentBlockDetected {
-			done = code
-		} else {
-			done = pushCommentToRight(code, 88)
-		}
-
-		fmt.Println(done)
+		done = pushCommentToRight(code, 88)
 		fmt.Fprintln(w, done)
 	}
 
-	err = w.Flush()
-	if err != nil {
-		return err
-	}
+	return w.Flush()
 
-	return scanner.Err()
 }
 
 func IsCXXFile(fileName string) bool {
@@ -169,24 +197,24 @@ func IsCXXFile(fileName string) bool {
 	return false
 }
 
-func GetClangFormatStylePath() (string, error) {
-	path, err := os.Executable()
-	if err != nil {
-		return "", err
-	}
+// func GetClangFormatStylePath() (string, error) {
+// 	path, err := os.Executable()
+// 	if err != nil {
+// 		return "", err
+// 	}
 
-	clangStylePath := filepath.Join(filepath.Dir(path), ".clang-format")
-	_, err = os.Stat(clangStylePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return "", fmt.Errorf(".clang-format cannot be found")
-		} else {
-			return "", fmt.Errorf(".clang-format cannot be accessed")
-		}
-	}
+// 	clangStylePath := filepath.Join(filepath.Dir(path), ".clang-format")
+// 	_, err = os.Stat(clangStylePath)
+// 	if err != nil {
+// 		if os.IsNotExist(err) {
+// 			return "", fmt.Errorf(".clang-format cannot be found")
+// 		} else {
+// 			return "", fmt.Errorf(".clang-format cannot be accessed")
+// 		}
+// 	}
 
-	return clangStylePath, nil
-}
+// 	return clangStylePath, nil
+// }
 
 func GetClangFormatExecutablePath() (string, error) {
 	path, err := os.Executable()
@@ -216,10 +244,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	clangStylePath, err = GetClangFormatStylePath()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// clangStylePath, err = GetClangFormatStylePath()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	entries, err := os.ReadDir(dirPath)
 	if err != nil {
@@ -228,6 +256,7 @@ func main() {
 
 	for _, entry := range entries {
 		if !entry.IsDir() && IsCXXFile(entry.Name()) {
+			Clean(dirPath, entry.Name())
 			Clean(dirPath, entry.Name())
 		}
 	}
